851f31c27ab1d9690e683a01270a3dd3a3b99a1a
Todd Krieger <twkriege@mtu.edu>
2021-09-22 12:29:00 -0400

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Code your solution here
+
+// const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby'];
+// const functionNames = ['findMatching', 'fuzzyMatch', 'matchName'];
+// console.log(drivers);
+// console.log(functionNames);
+
+
+
+function findMatching (array1, string1){
+    
+    let result1 = array1.filter(function (element){
+        return element.toLowerCase() === string1.toLowerCase();
+    });
+return result1;
+};
+
+function fuzzyMatch (array2, string2){
+    let len = string2.length;
+    let result2 = array2.filter(function(element){
+        return element.slice(0, len).toLowerCase() === string2.toLowerCase();
+    });
+return result2;
+};
+
+function matchName (array3, string3){
+    let result3 = array3.filter(function(element){
+        return element.name.toLowerCase() === string3.toLowerCase();
+    });
+return result3;
+};
